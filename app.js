d7294e8189f7999c2e6f30bc971b2ca9fd853407
bvishal8510 <baghel.vishal124124@gmail.com>
2020-10-08 19:16:11 +0530

    working of event module

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-// console.log(module.paths);
+// // // console.log(module.paths);
 
-const log = require('./logger');
+// // const log = require('./logger');  // ./ for user defined modules
 
-log("mine");
+// // // log("mine");
+
+// // const path = require('path');   // just name For built-in module
+// // const cpobj = path.parse(__filename);
+// // console.log(cpobj); 
+
+// const os = require('os');
+
+// const freem = os.freemem();
+// const totalm = os.totalmem();
+
+// console.log(`total memory : ${totalm}`);
+// console.log(`free memory ${freem}`);
+
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+emitter.on('messageLogged', function(){
+    console.log('message raised');
+})
+
+emitter.emit('messageLogged');
+
+
--- logger.js
+++ logger.js
@@ -1,8 +1,11 @@
-
+console.log(__filename);
+console.log(__dirname);
 const url = 'http://mylogger.io/logs';
 
 function log(message){
     console.log(message);
 }
 
-module.exports = log;
+module.exports=log;
+// exports.log = log;
+// module.exports.log = log;
