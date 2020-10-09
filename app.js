d9e46841fe0481e527449ed539866802245c75e5
bvishal8510 <baghel.vishal124124@gmail.com>
2020-10-09 18:10:29 +0530

    working with filesystem using node

--- app.js
+++ app.js
@@ -27,19 +27,28 @@ const { Socket } = require('dgram');
 // });
 
 
-const http = require('http');
-const server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.write(JSON.stringify([1,2,3]));
-        res.end();
-    }
+// const http = require('http');
+// const server = http.createServer((req, res)=>{
+//     if(req.url === '/'){
+//         res.write(JSON.stringify([1,2,3]));
+//         res.end();
+//     }
+// });
+
+// // server.on('connection', (socket) => {
+// //     console.log('new connection');
+// // })
+
+// server.listen(3000);
+
+// console.log("listening to port... ");
+
+const fs = require('fs');
+
+// const allfiles = fs.readdirSync('./');
+// console.log(allfiles);
+
+fs.readdir('./', (err, result)=>{
+    if(err) console.log(err);
+    else console.log(result);
 });
-
-// server.on('connection', (socket) => {
-//     console.log('new connection');
-// })
-
-server.listen(3000);
-
-console.log("listening to port... ");
-
