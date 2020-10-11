1063a3da2f2c0a3c4fc8edc861fec1bb8cc7ce08
bvishal8510 <baghel.vishal124124@gmail.com>
2020-10-11 12:47:35 +0530

    handle params in url

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,5 +9,18 @@ app.get('/courses', (req,res)=>{
     res.send([1,2,3]);
 });
 
+app.get('/courses/:id', (req, res) => {
+    res.send(req.params.id);
+});
+
+
+//for query string
+
+app.get('/man/:year/:month', (req, res) => {
+    // res.send(req.params.year);
+    res.send(req.query);
+});
+
+// dynamic port
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log(`listening to ${port}...`));
