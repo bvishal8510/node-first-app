67a1d72a04648486a633c7bf0727dd6255796192
bvishal8510 <baghel.vishal124124@gmail.com>
2020-10-11 16:24:58 +0530

    handling delete request

--- express-demo/index.js
+++ express-demo/index.js
@@ -11,6 +11,13 @@ const courses = [
     {id:4, name:'course 4'},
 ];
 
+function validate(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(course);
+}
+
 app.get('/', (req, res)=> {
     res.send('wllo World!!!');
 });
@@ -30,10 +37,11 @@ app.get('/courses/:id', (req, res) => {
 
 // post request
 app.post('/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    const {error, value} = schema.validate(req.body);         // result contains error and value(if valid request) 
+    // const schema = Joi.object({
+    //     name: Joi.string().min(3).required()
+    // });
+    // const {error, value} = schema.validate(req.body);         // result contains error and value(if valid request) 
+    const {error, value} = validate(req.body);
     if(error) res.status(400).send(error.details[0].message);
     else{ 
         const course = {
@@ -45,9 +53,35 @@ app.post('/courses', (req, res) => {
     }
 });
 
+//put request
+app.put('/courses/:id', (req, res) => {
+    // const schema = Joi.object({
+    //     name: Joi.string().min(3).required()
+    // });
+    // const {error, value} = schema.validate(req.body);         // result contains error and value(if valid request) 
 
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    const {error, value} = validate(req.body);
+    if(!course)  res.status(404).send(`Object Not found`);
+    else if(error) res.status(400).send(error.details[0].message);        
+    else 
+    {
+        course.name = req.body.name;
+        res.send(course);
+    }
+});
 
-
+//delete request
+app.delete('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course)  res.status(404).send(`Object Not found`);
+    else
+    {
+        const index = courses.indexOf(course);
+        courses.splice(index, 1);
+        res.send(courses);
+    }
+});
 
 //for query string
 app.get('/man/:year/:month', (req, res) => {
