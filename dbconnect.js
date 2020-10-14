bb8a2cd6c4c94659b3a5b494dae7f08b3f3fbbd1
bvishal8510 <baghel.vishal124124@gmail.com>
2020-10-14 18:59:57 +0530

    queries on mongodb

--- dbconnect.js
+++ dbconnect.js
@@ -12,9 +12,8 @@ const dbschema = new mongoose.Schema({
     date: { type: Date, default:Date.now},
     ispublished:Boolean,
 });
-
+const Course = mongoose.model('Course', dbschema);
 async function createCourse(){
-    const Course = mongoose.model('Course', dbschema);
     const course = new Course({
         name:'Angular Course',
         author:'Vishal',
@@ -25,4 +24,14 @@ async function createCourse(){
     const result = await course.save();
     console.log(result);
 };
-createCourse();
+// createCourse();
+
+async function findCourse(){
+    const courses = await Course
+        .find({author:'Vishal'})
+        .limit(10)                  //to set limit number of objects returned
+        .sort({name:1})         //set to -1 for desc order and can add more properties
+        .select({name:1, tags:1});    //to show only selected properties
+    console.log(courses);
+};
+findCourse();
