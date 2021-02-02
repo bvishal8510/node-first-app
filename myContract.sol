fb352a349643548eeda8c0fb3cce764b979ca12c
bvishal8510 <baghel.vishal124124@gmail.com>
2021-02-02 20:04:05 +0530

    creatig modifiers

--- myContract.sol
+++ myContract.sol
@@ -4,13 +4,28 @@ contract MyContract {
     int public peopleCount = 0;    // to count numebr of poeple
     mapping(int => Person) public people;
     
+    address owner;
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
     struct Person {
         string _firstName;
         string _lastName;
     }
     
-    function add(string _firstName, string _lastName) public {
-        peopleCount += 1;
+    function add(string _firstName, string _lastName) public onlyOwner{
+        increament();
         people[peopleCount] = Person(_firstName, _lastName);
+    }
+    
+    function increament() internal {
+        peopleCount += 1;
     }
 }
