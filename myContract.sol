c2aca9170a8f333a32739ce192b397d5336f45bc
bvishal8510 <baghel.vishal124124@gmail.com>
2021-02-04 20:11:15 +0530

    inheritance in solidity

--- myContract.sol
+++ myContract.sol
@@ -4,31 +4,30 @@ contract ERC20Token {
     string public name;
     mapping(address => uint256) public balances;
     
+    constructor(string memory _name) public {
+        name = _name;
+    }
+    
     function mint() public {
         balances[tx.origin] ++;  // use tx.origin in case of using one inside another
     }
 }
 
-contract MyContract {
+contract MyContract is ERC20Token{
     
-    mapping(address =>uint256) public balances;
+    string public symbol;
+    address[] public owners;
+    uint256 ownerCount;
     
-    address payable wallet;
-    address public token;
-
-    constructor(address payable _wallet, address _token) public {
-        wallet = _wallet;
-        token = _token;
+    constructor(string memory _name, string memory _symbol) ERC20Token(_name) public {
+        symbol = _symbol;
     }
     
-    function() external payable {
-        buyToken();
-    }
-    
-    function buyToken() public payable {
-        ERC20Token _token = ERC20Token(address(token));
-        _token.mint();
-        wallet.transfer(msg.value);
+    function mint() public {
+        super.mint();
+        owners.push(msg.sender);
+        ownerCount++;
+        
     }
     
 }
